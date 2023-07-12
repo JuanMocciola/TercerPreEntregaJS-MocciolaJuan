f5d55b7e69d3c6292afdef3cd48eaecf59c01099
JuanMocciola <juanmocciola@outlook.es>
2023-07-12 20:58:56 -0300

    Solucione ubicacion de imagenes en el array de objetos

--- JS/app.js
+++ JS/app.js
@@ -8,14 +8,14 @@ class Casco {
 };
 
 const cascos = [
-    new Casco(1, "../assets/images/casco1.png", "V1 Morphic Helmet", 864),
-    new Casco(2, "../assets/images/casco2.png", "V1 GOAT Vertigo Helmet", 912),
-    new Casco(3, "../assets/images/casco3.png", "Youth V1 Toxsyk Helmet", 499),
-    new Casco(4, "../assets/images/casco4.png", "V1 Bnkr Helmet", 1300),
-    new Casco(5, "../assets/images/casco5.png", "V1 Dpth Helmet", 999),
-    new Casco(5, "../assets/images/casco6.png", "Youth V1 Nuklr Helmet", 575),
-    new Casco(7, "../assets/images/casco7.png", "V3 RS Eyeris Helmet", 480),
-    new Casco(8, "../assets/images/casco8.png", "V3 RS Slait Helmet", 599),
+    new Casco(1, "./assets/images/casco1.png", "V1 Morphic Helmet", 864),
+    new Casco(2, "./assets/images/casco2.png", "V1 GOAT Vertigo Helmet", 912),
+    new Casco(3, "./assets/images/casco3.png", "Youth V1 Toxsyk Helmet", 499),
+    new Casco(4, "./assets/images/casco4.png", "V1 Bnkr Helmet", 1300),
+    new Casco(5, "./assets/images/casco5.png", "V1 Dpth Helmet", 999),
+    new Casco(5, "./assets/images/casco6.png", "Youth V1 Nuklr Helmet", 575),
+    new Casco(7, "./assets/images/casco7.png", "V3 RS Eyeris Helmet", 480),
+    new Casco(8, "./assets/images/casco8.png", "V3 RS Slait Helmet", 599),
 ];
 
 const productContainer = document.querySelector(".helmets-container");
